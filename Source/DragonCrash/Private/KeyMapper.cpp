5f75fafd28e316b2f0604d22bab329dc55ea7bdf
ashwin8394 <lily_33dan@live.com>
2017-04-06 14:26:22 -0700

    C++git add -A!

---
+++ Source/DragonCrash/Private/KeyMapper.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DragonCrash.h"
+#include "KeyMapper.h"
+
+KeyMapper::KeyMapper()
+{
+}
+
+KeyMapper::~KeyMapper()
+{
+}
---
+++ Source/DragonCrash/Public/KeyMapper.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class DRAGONCRASH_API KeyMapper
+{
+public:
+	KeyMapper();
+	~KeyMapper();
+};
