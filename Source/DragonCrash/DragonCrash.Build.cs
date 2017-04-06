335f836970a1c94550987d55c8d775ab70fe1c82
Edmond T Dooley <etdooley@crm.ucsc.edu>
2017-04-06 15:00:46 -0700

    Added Amazon Gamelift Server SDK to project.

--- Source/DragonCrash/DragonCrash.Build.cs
+++ Source/DragonCrash/DragonCrash.Build.cs
@@ -6,7 +6,7 @@ public class DragonCrash : ModuleRules
 {
 	public DragonCrash(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameLiftServerSDK" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
